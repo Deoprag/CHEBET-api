63c5407b4450ac939c3795114017a487c28fde86
Pedro Rocha <pdroesofiarabelo@gmail.com>
2023-08-09 10:21:10 -0300

    Update PilotController.java

--- chebet/src/main/java/com/deopraglabs/chebet/controller/PilotController.java
+++ chebet/src/main/java/com/deopraglabs/chebet/controller/PilotController.java
@@ -13,5 +13,52 @@ public class PilotController {
     @Autowired
     PilotService pilotService;
 
+    @PostMapping("/")
+    public ResponseEntity<String> registerPilot(@RequestBody(required = true) Map<String, String> requestMap) {
+        try {
+            return pilotService.register(requestMap);
+        } catch (Exception e) {
+            return ChebetUtils.getResponseEntity(Constants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Pilot>> getAllPilots() {
+        try {
+            return pilotService.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<List<Pilot>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Pilot> getPilot(@PathVariable int id) {
+        try {
+            return pilotService.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<Pilot>(new Pilot(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePilot(@PathVariable int id) {
+        try {
+            return pilotService.delete(id);
+        } catch (Exception e) {
+            return ChebetUtils.getResponseEntity(Constants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<String> updatePilot(@RequestBody(required = true) Map<String, String> requestMap) {
+        try {
+            return pilotService.update(requestMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ChebetUtils.getResponseEntity(Constants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
